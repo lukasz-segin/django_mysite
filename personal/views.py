bdc780e648a8140430f67c56664cddf03a389c41
lukasz-segin <lukasz-segin@o2.pl>
2021-10-07 16:50:56 +0200

    File changed by pre-commit, but i had to execute pre-commit run. Thats not what it suppose to be.

--- personal/views.py
+++ personal/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    print("dklsjfskdlfjsdklfjsdf") # sdkfjsdlfjsdlfkjdflkj
+    print("dklsjfskdlfjsdklfjsdf")  # sdkfjsdlfjsdlfkjdflkj
     print("dklsjfskdlfjsdklfjsdf")
-    print("dklsjfskdlfjsdklfjsdf")
+    print("dklsjfskdlfjsdklfjsdf")  # dskfjsdlkfj
     print("third print")
-    return render(request, 'personal/home.html')
+    return render(request, "personal/home.html")
+
 
 def contact(request):
-    return render(request, 'personal/basic.html', {'content':['If you would like to contact me', 'abc@o2.pl']})
+    return render(
+        request,
+        "personal/basic.html",
+        {"content": ["If you would like to contact me", "abc@o2.pl"]},
+    )
